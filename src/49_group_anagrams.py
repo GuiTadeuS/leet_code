63510a58a2ac887b142895860a574da8133b73f0
Guilherme Tadeu <tadeu.guilherme2002@gmail.com>
2023-11-24 09:24:00 -0300

    feat: add solution with sorting

--- src/49_group_anagrams.py
+++ src/49_group_anagrams.py
@@ -1,30 +1,17 @@
+from collections import defaultdict
+
+
 class Solution:
+    def groupAnagrams(self, strs):
+        anagram_map = defaultdict(list)
 
-    def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
-        dictionary = dict()
-        for letter_s in s:
-            if letter_s in dictionary:
-                dictionary[letter_s] += 1
-            else:
-                dictionary[letter_s] = 1
-        for letter_t in t:
-            if letter_t in dictionary:
-                dictionary[letter_t] -= 1
-        for value in dictionary.values():
-            if value != 0:
-                return False
-        return True
+        for palavra in strs:
+            sorted_word = str(sorted(palavra))
+            anagram_map[sorted_word].append(palavra)
 
-    def groupAnagrams(self, strs: list) -> list:
-
-        for index_i, value_i in enumerate(strs):
-            for index_j, value_j in enumerate(strs):
-                if index_i+1 < len(strs):
-                    print(self.isAnagram(strs[index_i], strs[index_j]))
+        return list(anagram_map.values())
 
 
 solution = Solution()
 strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
-solution.groupAnagrams(strs=strs)
+print(solution.groupAnagrams(strs=strs))
