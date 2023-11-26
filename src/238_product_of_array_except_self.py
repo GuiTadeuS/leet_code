99874d0d0a8cf215db2e73ee0afe1132ba1f9307
guilherme <tadeu.guilherme2002@gmail.com>
2023-11-26 00:17:12 -0300

    feat: add not optimal solution

--- src/238_product_of_array_except_self.py
+++ src/238_product_of_array_except_self.py
@@ -4,12 +4,18 @@ from typing import List
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         answer = []
-        for index, value in enumerate(nums):
-            if nums[index] != value:
-                result = nums[index]
+        for index_x, value_x in enumerate(nums):
+            stash = []
+            result = 1
+            for index_y, value_y in enumerate(nums):
+                if index_x != index_y:
+                    stash.append(value_y)
+            for value in stash:
+                result = result * value
+            answer.append(result)
+        return answer
 
 
 solution = Solution()
-nums = [1, 2, 3, 4]
-# print(len(nums))
+nums = [-1, 1, 0, -3, 3]
 print(solution.productExceptSelf(nums))
